1b0c5bcc114a02e4960a6f56862c038570cf7b34
Nayoung <iam149cm@knou.ac.kr>
2024-01-24 15:57:53 +0900

    initial commit

---
+++ 01_cleancodeJS/08_function/02_argument_parameter.js
@@ -0,0 +1,13 @@
+/** Arguments VS Parameters .. 매개변수, 인자, 인수 ....
+ 
+ * Arguments: real value. 함수를 호출할 때 실제로 넘겨주는 값
+ * Parameters: 함수에 정의되는 변수
+ */
+
+// 🚀 Parameters: 함수에 정의되는 변수. 형식을 갖춘 매개변수.
+function axios(url) { // url 은 parameter 
+    // ...
+}
+
+// 🚀 Arguments: Actual Parameter. 실제로 사용되는 인자. 인수.
+axios('https://www.naver.com') // 'https://www.naver.com' 은 argument
---
+++ 01_cleancodeJS/08_function/03_manage_args.js
@@ -0,0 +1,21 @@
+// 복잡한 인자 관리하기
+
+function toggleDisplay(isToggle) {
+    // ... some code
+}
+
+function sum (num1, num2) {
+    // ... some code
+}
+
+function getRandomNumber(min, max) {
+    // ... some code
+}
+
+function timer (start, stop, end) {
+    // ... some code
+}
+
+function genSquare(top, right, bottom, left) {
+    // ... some code
+}
