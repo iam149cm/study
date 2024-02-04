2e51e764ae5510937f5aedffd9f2d4b450b2559e
iam149cm <85501501+iam149cm@users.noreply.github.com>
2024-02-04 22:17:45 +0900

    initial commit

---
+++ 01_cleancodeJS/12_cooperation/03_style_guide.js
@@ -0,0 +1,18 @@
+/** Style guide
+ * - 네이밍 컨벤션을 포함하는 규칙을 위한 가이드라인으로 하나의 팀 혹은 집단을 위해 존재.
+ * - 협업에 도움을 주고 코드의 가독성을 높이기 위해 사용.
+ * 
+ * 1. 서로를 이해하기 위한 시간 절약
+ * 2. 코드 품질
+ * 3. 일관성
+ * 4. 가독성 향상
+ * 5. 유지보수 용이성
+ * 
+ * - 각 언어 별 구글 스타일 가이드가 있다. 
+ * - Google Style Guide
+ * - Airbnb JavaScript Style Guide
+ * - Rush stack style guide
+ * - MDN Web Docs
+ * - Prettier, eslint, editorconfig
+ * 
+ */
