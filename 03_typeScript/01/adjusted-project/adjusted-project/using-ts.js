5fa1dbc63aac68f43c1d0e1dfefea6cc8112f6cd
Nayoung <iam149cm@knou.ac.kr>
2025-03-20 20:57:25 +0900

    initial commit

---
+++ 03_typeScript/01/adjusted-project/adjusted-project/using-ts.js
@@ -0,0 +1,9 @@
+var button = document.querySelector("button");
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
+function add(num1, num2) {
+    return num1 + num2;
+}
+button.addEventListener("click", function () {
+    console.log(add(+input1.value, +input2.value)); // + 를 붙임으로써 숫자로 변환 가능함
+});
