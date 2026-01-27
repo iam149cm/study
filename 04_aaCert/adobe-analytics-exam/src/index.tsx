2d17038c0243c6df32825f732e001bb3e7097700
Nayoung <iam149cm@knou.ac.kr>
2026-01-27 17:19:23 +0900

    update basename

--- 04_aaCert/adobe-analytics-exam/src/index.tsx
+++ 04_aaCert/adobe-analytics-exam/src/index.tsx
@@ -8,18 +8,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-// GitHub Pages용 basename 설정
-const isProduction = process.env.NODE_ENV === "production";
-const basename = isProduction
-  ? process.env.REACT_APP_BASENAME || "/"
-  : "/";
-
-console.log( "NODE_ENV >> ", process.env.NODE_ENV);
-console.log( "REACT_APP_BASENAME >> ", process.env.REACT_APP_BASENAME);
-
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
